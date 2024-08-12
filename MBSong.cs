31b14c54128429660ddf8e69f5de99bad8ec2841
Trevor <me@trev.app>
2024-08-12 18:58:23 -0400

    Use display artist for tracks
    Can be a listing of artists like before, but can also not be if the user changes it

--- MBSong.cs
+++ MBSong.cs
@@ -16,7 +16,7 @@ namespace MusicBeePlugin
             Name = mbApiInterface.Library_GetFileTag(sourceFileUrl, MetaDataType.TrackTitle);
             AlbumName = mbApiInterface.Library_GetFileTag(sourceFileUrl, MetaDataType.Album);
             AlbumArtist = mbApiInterface.Library_GetFileTag(sourceFileUrl, MetaDataType.AlbumArtist);
-            Artist = mbApiInterface.Library_GetFileTag(sourceFileUrl, MetaDataType.Artists);
+            Artist = mbApiInterface.Library_GetFileTag(sourceFileUrl, MetaDataType.Artist);
 
             // Durations are in MM:SS format and TimeSpan doesn't support parsing something over 59 minutes so this is a janky workaround
             string strDuration = mbApiInterface.Library_GetFileProperty(sourceFileUrl, FilePropertyType.Duration);
